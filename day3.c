6f0fd0bf72b0d87e068251dcf8e2a1cb2b5118da
ashishsaini576 <ashishsaini9953@gmail.com>
2026-07-13 12:34:13 +0530

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("enter first number: ");
+    scanf("%d", &a);
+
+    printf("enter second number: ");
+    scanf("%d", &b);
+
+    printf("mul = %d", a * b);
+    return 0;
+
+}
